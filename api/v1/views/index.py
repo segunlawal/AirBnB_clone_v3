b73b48121701a8a6253400aebcc85399fa3aec0f
Segun <lawalsegun72@gmail.com>
2023-07-29 15:32:27 -0700

    add model imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def status():
